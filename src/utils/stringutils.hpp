ad7bc5ba916286e307eda08e03a930b4a4a63e9d
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2023-08-16 19:06:17 +0800

    Create new methods for stringutils
    * new "startsWith()" and "endsWith()" method

--- src/utils/stringutils.hpp
+++ src/utils/stringutils.hpp
@@ -8,4 +8,10 @@ int stringEmpty(std::string t_s = "");
 std::string quoteString(std::string s = "");
 std::string encloseString(std::string s = "", std::string op = "", std::string cl = "");
 
+// Source: https://stackoverflow.com/a/42844629
+int startsWith(const std::string& str, const std::string& suffix);
+
+// Source: https://stackoverflow.com/a/42844629
+int endsWith(const std::string& str, const std::string& prefix);
+
 #endif // STRINGUTILS_HPP
