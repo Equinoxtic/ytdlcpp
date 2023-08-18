0ee86f41b447872ca3fc223d65c94b8287b7c22b
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2023-08-18 13:09:22 +0800

    Update playmode.cpp
    General overhaul from pre-0.2

--- src/states/playmode.cpp
+++ src/states/playmode.cpp
@@ -2,10 +2,15 @@
 #include "../utils/stringutils.hpp"
 #include "../utils/sys.hpp"
 #include "../ui/option.hpp"
+#include "../ui/bar.hpp"
+#include "../list.hpp"
+#include "../logging.hpp"
 #include<iostream>
 #include<string>
 #include<sstream>
 #include<vector>
+
+static const int defaultBarSize = 100;
 
 static void generateControls()
 {
@@ -39,6 +44,54 @@ static void generateControls()
 	std::cout << "\n";
 }
 
+static void generateBanner()
+{
+	std::cout << "PLAYMODE - Playing Video...\n\n";
+	createBar(defaultBarSize);
+	std::cout << "\n\n";
+}
+
+static void generateUsage()
+{
+	std::vector<std::string> usageList = {
+		"Play videos and audios. (Uses FFplay)",
+		"Look through the directory listing to find and choose the file you want to play.",
+		"Supported formats are: [*.mp4, *.mov, *.ogg, *.mp3, *.wav]",
+		"To exit, type in \"exit\""
+	};
+	
+	std::cout << "\n";
+	createBar(100);
+	std::cout << "\n\n-- USAGE OF PLAYMODE --\n\n";
+	createBulletList(usageList);
+	std::cout << "\n";
+}
+
+static void createListingHeader()
+{
+	std::cout << "[ Listing the local directory for video/audio files... ]";
+	std::cout << "\n\n-- LOCAL DIRECTORY LISTING --\n\n";
+}
+
+static int listValidFiles(std::vector<std::string>& extList)
+{
+	if (extList.empty()) {
+		return 1;
+	} else {
+		std::ostringstream oss_ext;
+		for (size_t i = 0; i < extList.size(); ++i) {
+			#ifndef _WIN32
+			oss_ext << "ls -p ./local/*" << extList[i] << " | egrep -v /$"
+			#else
+			oss_ext << "dir /B .\\local\\*" << extList[i];
+			#endif
+			sysExecute(oss_ext.rdbuf()->str());
+			oss_ext.rdbuf()->str(std::string());
+		}
+	}
+	return 0;
+}
+
 int runPlayMode()
 {
 	std::string coption = "";
@@ -52,35 +105,45 @@ int runPlayMode()
 	};
 	int isValidExt = false;
 	
-	std::cout << "Listing the local directory for video/audio files...\n\n";
+	sysClearScreen();
 	
-	#ifndef _WIN32
-	sysExecute("ls ./local/*.mov && ls ./local/*.mp4");
-	#else
-	sysExecute("dir .\\local\\*.mov & dir .\\local\\*.mp4");
-	#endif
-	
-	std::cout << "\n: ";
-	
-	std::getline(std::cin, coption);
-	ioption = coption;
-	
-	for (size_t i = 0; i < validFileExt.size(); ++i) {
-		if (endsWith(ioption, validFileExt[i])) {
-			isValidExt = true;
+	generateBanner();
+	createListingHeader();
+	listValidFiles(validFileExt);
+	generateUsage();
+
+	while (compareString(ioption, "exit") != 0 ) {
+		std::cout << "\n[prompt/play]:~$ ";
+		
+		std::getline(std::cin, coption);
+		ioption = coption;
+		
+		for (size_t i = 0; i < validFileExt.size(); ++i) {
+			if (endsWith(ioption, validFileExt[i])) {
+				isValidExt = true;
+			} else {
+				isValidExt = false;
+			}
+		}
+		
+		if (!(stringEmpty(ioption)) == 0) {
+			if (!(compareString(ioption, "exit")) == 0) {
+				if (isValidExt) {
+					std::ostringstream oss;
+					generateControls();
+					oss << "ffplay -loglevel error -noborder -x 1280 -y 720 -loop 0 \"./local/" << ioption << "\"";
+					sysExecute(oss.rdbuf()->str());
+				} else {
+					return 1;
+				}
+			} else if (compareString(ioption, "list") == 0) {
+				createListingHeader();
+				listValidFiles(validFileExt);
+			}
+		} else {
+			logMessage("Retrying. String literal cannot be empty or null.", "warn");
 		}
 	}
-	
-	if (isValidExt) {
-		std::ostringstream oss;
-		generateControls();
-		oss << "ffplay -loglevel quiet -noborder -x 1280 -y 720 -loop 0 \"./local/" << ioption << "\"";
-		sysExecute(oss.rdbuf()->str());
-	} else {
-		return 1;
-	}
-	
-	sysPause(true, false);
-	
+
 	return 0;
 }
